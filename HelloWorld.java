8e96d1feb6d47052203937020f150984a39a04dd
buildwithroop <wildforcerdb@gmail.com>
2026-09-06 07:17:06 +0530

    Modified Java code

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class FirstProgram
 {
-    pubilc static void main 
+    pubilc static void main(String[] args) 
     {
         System.out.printf("Hello World");
     }
